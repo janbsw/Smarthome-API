df832a145598f5554cc4f64561fbc9f77ac3476d
janbsw <67024007+janbsw@users.noreply.github.com>
2023-02-15 14:33:07 +0100

    async sleep

--- src/microcontroller/con_index.js
+++ src/microcontroller/con_index.js
@@ -6,6 +6,9 @@ const board = new Board({
     baudrate:"9600"
 });
 
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+
 // The board's pins will not be accessible until
 // the board has reported that it is ready
 function init(){
@@ -14,14 +17,17 @@ function init(){
       });
 }
 
-function switchlamp(ID){
+async function switchlamp(ID){
     console.log("Lamp switched with ID:" + ID);
     let bits = binaryarray(ID)
     board.digitalWrite(12, 1)
+    sleep(250)
     for(i = 0; i>8; i++){
         board.digitalWrite(12, Number(bits[i]))
+        sleep(250)
     }
-    board.digitalWrite(12, 1)
+    board.digitalWrite(12, 0)
+    sleep(250);
 }
 
 module.exports = {init, switchlamp};
